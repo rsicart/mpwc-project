0e6eae2cb2da2217f7899c63320af0310276f314
R. Sicart <mpwcproject@gmail.com>
2012-11-21 19:56:16 +0100

    New portlet for managing projects

---
+++ docroot/WEB-INF/src/com/mpwc/project/ProjectPortlet.java
@@ -0,0 +1,11 @@
+package com.mpwc.project;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class ProjectPortlet
+ */
+public class ProjectPortlet extends MVCPortlet {
+ 
+
+}
